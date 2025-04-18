6c2e1e77a0e1755f292c6307538250761fdf16a9
sajib689 <jibonraja11@gmail.com>
2025-04-18 15:27:36 +0600

    Comment out the console log for HouseRent total calculation and update HouseRent2 incrementYearly console log

--- parice.js
+++ parice.js
@@ -7,7 +7,7 @@ HouseRent.prototype.calculateTotalRent = function () {
   return `In this month, you will pay ${this.rent} for ${this.months} months. Total rent is ${this.rent * this.months}`;
 }
 
-console.log(new HouseRent(1000, 12).calculateTotalRent()) // 12000
+// console.log(new HouseRent(1000, 12).calculateTotalRent()) // 12000
 
 const HouseRent2 = function (amount) {
     this.amount = amount;
@@ -18,4 +18,4 @@ HouseRent2.prototype.incrementYearly = function () {
 }
 const myHouse = new HouseRent2(1000);
 
-console.log(myHouse.incrementYearly(1200));
+console.log(myHouse.incrementYearly());
