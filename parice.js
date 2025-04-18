f8494a2693a2607f579e882331cf4d8c31d45ff0
sajib689 <jibonraja11@gmail.com>
2025-04-18 14:57:05 +0600

    Update calculateTotalRent method to include rent increment details

--- parice.js
+++ parice.js
@@ -4,7 +4,7 @@ const HouseRent = function (rent, months) {
 }
 
 HouseRent.prototype.calculateTotalRent = function () {
-  return `In this month, you will pay ${this.rent} for ${this.months} months. Total rent is ${this.rent * this.months}`;
+  return `In this month, you will pay ${this.rent} for ${this.months} months. Total rent is ${this.rent * this.months} after 12 months of rent increment ${this.month * 500}`;
 }
 
 console.log(new HouseRent(1000, 12).calculateTotalRent()) // 12000
