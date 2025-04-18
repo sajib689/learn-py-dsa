74c29f900119258a69421ecbb0a2f6e86982be78
sajib689 <jibonraja11@gmail.com>
2025-04-18 15:10:15 +0600

    Add incrementYearly method to HouseRent2 class for yearly rent calculation

--- parice.js
+++ parice.js
@@ -12,7 +12,10 @@ console.log(new HouseRent(1000, 12).calculateTotalRent()) // 12000
 const HouseRent2 = function (amount) {
     this.amount = amount;
 }
-
+HouseRent2.prototype.incrementYearly = function () {
+    this.amount += 1000;
+    return `In this month, you will pay ${this.amount} for 12 months. Total rent is ${this.amount * 12}`;
+}
 const myHouse = new HouseRent2(1000);
 
-console.log(myHouse);
+console.log(myHouse.incrementYearly(1200));
