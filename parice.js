242b37c492f71f0440e749934cfd57b8b5f58341
sajib689 <jibonraja11@gmail.com>
2025-04-18 14:54:07 +0600

    Update calculateTotalRent method to include detailed output message

--- parice.js
+++ parice.js
@@ -4,7 +4,8 @@ const HouseRent = function (rent, months) {
 }
 
 HouseRent.prototype.calculateTotalRent = function () {
-  return this.rent * this.months;
+  return `In this month, you will pay ${this.rent} for ${this.months} months. Total rent is ${this.rent * this.months}`;
 }
 
 console.log(new HouseRent(1000, 12).calculateTotalRent()) // 12000
+
